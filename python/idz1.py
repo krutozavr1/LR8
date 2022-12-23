18dd04420c7afef8b98df45bb1050869d5632e7f
Иван <ivannsharipkov@gmail.com>
2022-12-23 23:29:01 +0300

    Update idz1.py

--- python/idz1.py
+++ python/idz1.py
@@ -5,12 +5,7 @@ import random
 import sys
 
 if __name__ == '__main__':
-    tour1 = []
-    tour2 = []
-    for i in range(0, 26):
-        tour1.append(random.randrange(0, 5))
-        tour2.append(random.randrange(0, 5))
+    t1 = (random.randrange(0, 5) for i in range(0, 26))
+    t2 = (random.randrange(0, 5) for i in range(0, 26))
 
-    tour1, tour2 = tuple(tour1), tuple(tour2)
-
-    print(f"Overall summ: {sum(tour1) + sum(tour2)}")
+    print(f"Overall summ: {sum(t1) + sum(t1)}")
